9c63b781160551e81f7609781c39c5f2245f021d
Mehdi Bourahla <mehdibrhl4@gmail.com>
2022-05-11 09:18:18 +0100

    ⚡️ Code Splitting and Dynamic Imports

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,7 +2,7 @@ import Vue from "vue";
 import { createInertiaApp } from "@inertiajs/inertia-vue";
 
 createInertiaApp({
-    resolve: (name) => require(`./Pages/${name}`),
+    resolve: async (name) => (await import(`./Pages/${name}`)).default,
     setup({ el, App, props, plugin }) {
         Vue.use(plugin);
 
