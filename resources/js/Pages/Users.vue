3671baad0c8de7f4b83500d4e374d6edf3fc4740
Mehdi Bourahla <mehdibrhl4@gmail.com>
2022-05-11 10:51:20 +0100

    ✨ Setup Pagination

--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -6,22 +6,25 @@
         Plenty of users
 
         <ul>
-            <li v-for="user in users" :key="user.id">
+            <li v-for="user in users.data" :key="user.id">
                 {{ user.name }}
             </li>
         </ul>
+        <Pagination :pages="users.links" />
     </div>
 </template>
 
 <script>
 import Layout from "../Shared/Layout.vue";
+import Pagination from "../Shared/Pagination.vue";
 
 export default {
+    components: { Pagination },
     layout: Layout,
     props: {
         users: {
-            type: Array,
-            default: () => [],
+            type: Object,
+            default: null,
         },
     },
 };
---
+++ resources/js/Shared/Pagination.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <Link
+            v-for="page in pages"
+            :key="page.url"
+            :href="page.url"
+            v-html="page.label"
+        >
+        </Link>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        pages: {
+            type: Array,
+            default: () => [],
+        },
+    },
+};
+</script>
+
+<style></style>
